952faebf0e2027618127b2e2972d609d80d1652b
gniang <jing.art@gmail.com>
2020-01-06 21:35:58 +0900

    プロジェクト ファイルを追加します。

---
+++ VideoStitchSample/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.preview = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.resultPreview = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // preview
+            // 
+            this.preview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.preview.Location = new System.Drawing.Point(3, 3);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(466, 703);
+            this.preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.preview.TabIndex = 0;
+            this.preview.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.5F));
+            this.tableLayoutPanel1.Controls.Add(this.preview, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.resultPreview, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblStatus, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1260, 740);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // resultPreview
+            // 
+            this.resultPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultPreview.Location = new System.Drawing.Point(475, 3);
+            this.resultPreview.Name = "resultPreview";
+            this.resultPreview.Size = new System.Drawing.Size(782, 703);
+            this.resultPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resultPreview.TabIndex = 1;
+            this.resultPreview.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 712);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Re Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblStatus.Location = new System.Drawing.Point(475, 709);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(38, 16);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "      ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1260, 740);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPreview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox preview;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox resultPreview;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ VideoStitchSample/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using NReco.VideoConverter;
+using NReco.VideoInfo;
+using System.IO;
+using System.Diagnostics;
+using OpenCvSharp.Extensions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await run();
+        }
+
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            await run();
+        }
+
+
+        private async Task run()
+        {
+            const string videoPath = "test.mp4";
+            const string resultBmpPath = "result.bmp";
+
+            var inputs = new List<Mat>();
+            var resultPanorama = new Mat();
+            var stitcher = OpenCvSharp.Stitcher.Create(Stitcher.Mode.Panorama);
+
+            // プレビュー表示しながらStitch用の画像リスト(List<Mat>)保存
+            // Streamで処理する方法ないのかな？　元動画が長いとキツい？
+            foreach (var bmp in GetThumbnails(videoPath))
+            {
+                var oldImage = preview.Image;
+                await Task.Delay(100);
+                preview.Image = bmp;
+
+                oldImage?.Dispose();
+
+                inputs.Add(BitmapConverter.ToMat(bmp));
+            }
+
+            lblStatus.Text = "画像統合中・・・";
+
+            // 画像リストからパノラマ画像作成
+            var resultBmp = await Task.Run(() =>
+            {
+                stitcher.Stitch(inputs, resultPanorama);
+                inputs.ForEach(x => x.Dispose());
+                return resultPanorama.ToBitmap();
+            });
+
+            // 結果のプレビュー表示
+            {
+                var oldImage = resultPreview.Image;
+                resultPreview.Image = resultBmp;
+                oldImage?.Dispose();
+            }
+            lblStatus.Text = "";
+
+            await Task.Run(() =>
+            {
+                resultPanorama.SaveImage(resultBmpPath);
+                resultPanorama.Dispose();
+            });
+        }
+
+
+        /// <summary>
+        /// だいたい0.3秒おきの動画サムネイルを取得する
+        /// </summary>
+        /// <param name="videoPath">ビデオファイルパス</param>
+        /// <returns></returns>
+        public static IEnumerable<Bitmap> GetThumbnails(string videoPath)
+        {
+            // メタ情報で再生時間取得
+            var ffprob = new NReco.VideoInfo.FFProbe();
+            var info = ffprob.GetMediaInfo(videoPath);
+            var duration = info.Duration;
+
+            var videoConv = new NReco.VideoConverter.FFMpegConverter();
+
+            // 最短0.3秒おき、最大100分割
+            var skipSec = duration.TotalSeconds < 30 ?
+                          0.3f : (float)(duration.TotalSeconds / 100);
+
+            var frameSec = 0f;
+            while((float)duration.TotalSeconds > frameSec)
+            {
+                // Bitmapリソースは受信側で開放する。
+                var jpegStream = new MemoryStream();
+                videoConv.GetVideoThumbnail(videoPath, jpegStream, frameSec);
+                yield return (Bitmap)Image.FromStream(jpegStream);
+                frameSec += skipSec;
+            }
+        }
+
+    }
+}
